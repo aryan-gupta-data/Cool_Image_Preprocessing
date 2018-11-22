0f8af21bfb514bd5e9c521f4d91668e72283e393
aryan-gupta-data <41750405+aryan-gupta-data@users.noreply.github.com>
2018-11-21 20:53:47 -0500

    Update try_rescale_imageio.py

--- try_rescale_imageio.py
+++ try_rescale_imageio.py
@@ -7,49 +7,6 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 import skimage
 from numpy import matlib as mb
 import imageio
-
-# Load training images and labels
-x_train_list = np.load('train_images.npy', encoding='bytes')
-train_labels = pd.read_csv('train_labels.csv')
-x_test_list = np.load('test_images.npy', encoding='bytes')
-
-#imageio.mimwrite('./images', x_train_list)
-
-
-import numpy as np
-import matplotlib as mpl
-# mpl.use('Agg')
-import matplotlib.pyplot as plt
-
-img = np.ones((100, 200))
-img[25:75, 50:150] = 0
-
-fig = plt.figure()
-ax = fig.gca()
-
-ax.imshow(img)
-ax.axis('tight')
-
-plt.subplots_adjust(0,0,1,1,0,0)
-
-plt.show()
-
-data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
-w, h = fig.canvas.get_width_height()
-data = data.reshape((h, w, 3))
-
-plt.close()
-
-#--------------------------------------------------------------------------------------------
-import numpy as np
-import pandas as pd
-import random
-import matplotlib
-import matplotlib.pyplot as plt
-get_ipython().run_line_magic('matplotlib', 'inline')
-import skimage
-from numpy import matlib as mb
-import imageio
 import cv2
 import imageio as iio
 from skimage import filters
@@ -109,32 +66,32 @@ for idx, category in enumerate(y_categories):
 x_train /= 255
 x_test /= 255
 
-center_ind_list_row = []
-center_ind_list_col = []
-for i in range(x_train.shape[0]):
-    img = (x_train[i].reshape(100,100))
-    center_ind_list_col.append((ndimage.measurements.center_of_mass(img)[0]))
-    center_ind_list_row.append((ndimage.measurements.center_of_mass(img)[1]))
-
-center_ind_list_row = np.array(center_ind_list_row)
-center_ind_list_col = np.array(center_ind_list_col)
-
-for j in range(10):
-    img = (x_train[j].reshape(100,100))
-    plt.imshow(img)
-    plt.axis("off")
-    plt.show()
-    centre_ind = ndimage.measurements.center_of_mass(img)
-    
-    size_of_crop = 30
-    centre_row = int(round(centre_ind[1]))
-    centre_col = int(round(centre_ind[0]))
-    #img_crop = img[46-20:46+20,39-20:39+20]
-    img_crop = img[(centre_row-size_of_crop):(centre_row+size_of_crop),(centre_col-size_of_crop):(centre_col+size_of_crop)]
-    plt.imshow(img_crop)
-    plt.axis("off")
-    plt.show()
-    print ("IMAGE # = ",j)
+#center_ind_list_row = []
+#center_ind_list_col = []
+#for i in range(x_train.shape[0]):
+#    img = (x_train[i].reshape(100,100))
+#    center_ind_list_col.append((ndimage.measurements.center_of_mass(img)[0]))
+#    center_ind_list_row.append((ndimage.measurements.center_of_mass(img)[1]))
+#
+#center_ind_list_row = np.array(center_ind_list_row)
+#center_ind_list_col = np.array(center_ind_list_col)
+#
+#for j in range(10):
+#    img = (x_train[j].reshape(100,100))
+#    plt.imshow(img)
+#    plt.axis("off")
+#    plt.show()
+#    centre_ind = ndimage.measurements.center_of_mass(img)
+#    
+#    size_of_crop = 30
+#    centre_row = int(round(centre_ind[1]))
+#    centre_col = int(round(centre_ind[0]))
+#    #img_crop = img[46-20:46+20,39-20:39+20]
+#    img_crop = img[(centre_row-size_of_crop):(centre_row+size_of_crop),(centre_col-size_of_crop):(centre_col+size_of_crop)]
+#    plt.imshow(img_crop)
+#    plt.axis("off")
+#    plt.show()
+#    print ("IMAGE # = ",j)
 
 img = (x_train[10].reshape(100,100))
 plt.imshow(img)
@@ -180,50 +137,3 @@ img_crop = img[(40-size_of_crop):(60+size_of_crop),(20-size_of_crop):(40+size_of
 plt.imshow(img_crop)
 plt.axis("off")
 plt.show()
-
-img_crop = img[0:20,80:100]
-plt.imshow(img_crop)
-plt.axis("off")
-plt.show()
-
-
-
-#ret,thresh = cv2.threshold(img,127,255,0)
-#thresh, im_bw = cv2.threshold(img, 127, 255, 0)
-#contours,hierarchy = cv2.findContours(im_bw, 1, 2)
-
-x,y,w,h = cv2.boundingRect()
-cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
